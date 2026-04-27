28ef5a31c45eca8e218c6817f1cde91e1d1735ec
Scott Kaufman <scottring@hotmail.com>
2026-04-27 13:50:46 -0400

    chore(family): export FamilyBadge from barrel
    Lets consumers import via '@/components/family' alongside other
    family-domain components.

--- src/components/family/index.ts
+++ src/components/family/index.ts
@@ -1,3 +1,4 @@
 export { AssigneeAvatar } from './AssigneeAvatar'
 export { AssigneeDropdown } from './AssigneeDropdown'
+export { FamilyBadge } from './FamilyBadge'
 export { MultiAssigneeDropdown } from './MultiAssigneeDropdown'
